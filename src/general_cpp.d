302c86d84aef344816ba7dd9055e56757e936377
icosivi <federico.siviero@edu.unito.it>
2024-06-12 15:52:46 +0200

    Generalization and simpler tct positions

--- src/general_cpp.d
+++ src/general_cpp.d
@@ -1,6 +1,6 @@
 
 # DO NOT DELETE
 
-src/general_cpp.so: /home/federico/Scrivania/ANALISI/Rsd/analysis_simple/include/general.hpp
-src/general_cpp.so: /home/federico/root/mybuild/include/RVersion.h /home/federico/root/mybuild/include/RConfig.h /home/federico/root/mybuild/include/TClass.h /home/federico/root/mybuild/include/TDictAttributeMap.h /home/federico/root/mybuild/include/TInterpreter.h /home/federico/root/mybuild/include/TROOT.h /home/federico/root/mybuild/include/TBuffer.h /home/federico/root/mybuild/include/TMemberInspector.h /home/federico/root/mybuild/include/TError.h /home/federico/root/mybuild/include/RtypesImp.h /home/federico/root/mybuild/include/TIsAProxy.h /home/federico/root/mybuild/include/TFileMergeInfo.h /home/federico/root/mybuild/include/TCollectionProxyInfo.h /home/federico/root/mybuild/bin/rootcling
-general_cpp__ROOTBUILDVERSION= 6.16/00
+src/general_cpp.so: /home/tb_pc/Desktop/to_git/BetaAnalysis/include/general.hpp
+src/general_cpp.so: /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/RVersion.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/RConfig.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TClass.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TDictAttributeMap.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TInterpreter.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TROOT.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TBuffer.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TMemberInspector.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TError.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/RtypesImp.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TIsAProxy.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TFileMergeInfo.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/include/TCollectionProxyInfo.h /home/tb_pc/Desktop/DigiDAQ/ROOT/install/bin/rootcling
+general_cpp__ROOTBUILDVERSION= 6.26/10
